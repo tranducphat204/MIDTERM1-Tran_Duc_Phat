0e5171abc5a0e7e835748f9744d50d9434236564
phatdeptraiii <vitphat350@gmail.com>
2024-05-25 09:56:02 +0700

    done ex3

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -18,7 +18,9 @@ function App() {
     <div id="app">
       <h1>PNV Team !!</h1>
       <p>Here are some PNV trainers and educators, do you know them?</p>
-      <User /> {/* How to loop on ALL_PNV_TEACHERS list ? */}
+      {ALL_PNV_TEACHERS.map((index) => (
+        <User key={index} user={teacher} />
+      ))}
     </div>
   );
 }
