75f450f30b91515bd111fa7bf732db95b1ecb3b2
phatdeptraiii <vitphat350@gmail.com>
2024-05-25 10:37:06 +0700

    done ex4

--- exercise-4/src/components/Place.jsx
+++ exercise-4/src/components/Place.jsx
@@ -1,9 +1,9 @@
-export default function Place() {
+export default function Place({ id, title, image: { src, alt } }) {
   return (
-    <li key="the place id" className="place-item">
+    <li key={id} className="place-item">
       <button>
-        <img src="the place image src" alt="the place image alt" />
-        <h3>"the place title"</h3>
+        <img src={src} alt={alt} />
+        <h3>{title}</h3>
       </button>
     </li>
   );
